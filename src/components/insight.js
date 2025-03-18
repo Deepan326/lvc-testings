e0bc37a8592997cec96d4561db8cd1beac7b3eae
William Lyman <luke.sauls279@gmail.com>
2025-03-18 12:59:59 -0700

    fixed latest news images

--- src/components/insight.js
+++ src/components/insight.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 
 import "swiper/css";
 import "swiper/css/pagination";
-import 'swiper/css/navigation';
+import "swiper/css/navigation";
 import "../style.css";
-import { Pagination ,Navigation } from "swiper/modules";
+import { Pagination, Navigation } from "swiper/modules";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -15,10 +15,10 @@ import { useNavigate } from "react-router-dom";
 
 const Insight = () => {
   const navigate = useNavigate();
-    
+
   const handleNavigation = (path) => {
-      navigate(path);
-      window.scrollTo(0, 0);
+    navigate(path);
+    window.scrollTo(0, 0);
   };
 
   const [showAllServices, setShowAllServices] = useState(false);
@@ -108,8 +108,6 @@ const Insight = () => {
       description: "TAKEAWAYS FROM A DIRECTORY",
     },
   ];
- 
-  
 
   return (
     <main>
@@ -121,7 +119,9 @@ const Insight = () => {
             <hr className="underline" />
             <h1 className="insight-title">Where Ideas Meet Action</h1>
             <h5>Insights that shape the future.</h5>
-             <Link to="/contact" className="btn btn-primary-insight">Let's Talk</Link>
+            <Link to="/contact" className="btn btn-primary-insight">
+              Let's Talk
+            </Link>
           </div>
         </div>
       </div>
@@ -176,196 +176,198 @@ const Insight = () => {
       {/* Latest News Section */}
       <div className="isight-wrapper">
         <h1>Updated with Latest news</h1>
-        <Swiper
-          slidesPerView={3}
-          spaceBetween={30}
-          pagination={{ clickable: true }}
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          {img.map((img, index) => (
-            <SwiperSlide key={index}>
-              <div className="swiper-image-container position-relative">
-                <img
-                  src={img.src}
-                  alt={img.alt}
-                  className="w-100 swiper-image"
-                />
-                <div className="overlay-text overlay-text-insight">
-                  <h1 className="getupdate-number">{img.number}</h1>
-                  <h2>{img.description}</h2>
-                  <h3>{img.title}</h3>
-                  <h2
-                    className="insight-readmore-cta"
-                    style={{
-                      color: "white",
-                      paddingTop: "15px",
-                      cursor: "pointer",
-                      display: "flex",
-                      alignItems: "center",
-                      flexWrap:"wrap",
-                      gap: "8px",
-                    }}
-                  >
-                    Read More <FontAwesomeIcon icon={faArrowRight} />
-                  </h2>
-                </div>
+        <div className="latest-news-container">
+          {img.map((img) => (
+            <div className="swiper-image-container position-relative">
+              <img src={img.src} alt={img.alt} className="w-100 swiper-image" />
+              <div className="overlay-text overlay-text-insight">
+                <h1 className="getupdate-number">{img.number}</h1>
+                <h2>{img.description}</h2>
+                <h3>{img.title}</h3>
+                <h2
+                  className="insight-readmore-cta"
+                  style={{
+                    color: "white",
+                    paddingTop: "15px",
+                    cursor: "pointer",
+                    display: "flex",
+                    alignItems: "center",
+                    flexWrap: "wrap",
+                    gap: "8px",
+                  }}
+                >
+                  Read More <FontAwesomeIcon icon={faArrowRight} />
+                </h2>
               </div>
-            </SwiperSlide>
+            </div>
           ))}
-        </Swiper>
+        </div>
       </div>
 
       {/* Trusted by Companies Section */}
 
-<div>
-
-<Swiper
-        slidesPerView={2}
-        spaceBetween={50}
-        navigation={{ clickable: true,}}
-        modules={[Pagination, Navigation]}
-        
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className="pt-customer-review">
-            <p className="pt-customer-review-description">
-              LVC Solutions LLC completely transformed our operations. Their
-              team delivered a seamless solution tailored to our needs, ensuring
-              efficiency and scalability. From planning to execution, every step
-              was handled with professionalism and care. Their commitment to
-              excellence is unmatched. Thanks to them, we’ve gained a
-              competitive edge in the market. Highly recommend their services!
-            </p>
-            <p className="pt-customer-review-caption">
-             <strong style={{color:"black"}}>John S.,</strong>  CEO, E-commerce Business
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="pt-customer-review">
-            <p className="pt-customer-review-description">
-              Partnering with LVC Solutions was the best decision we made for
-              our company. Their team not only understood our requirements but
-              also delivered innovative solutions to optimize our processes.
-              Their responsiveness and expertise set them apart from others.
-              We’ve seen remarkable improvements in productivity and customer
-              satisfaction. Truly a game-changer for us!
-            </p>
-            <p className="pt-customer-review-caption">
-                <strong style={{color:"black"}}> Emily K., </strong> CTO, Healthcare Firm
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="pt-customer-review">
-            <p className="pt-customer-review-description">
-              We trusted LVC Solutions to revamp our IT infrastructure, and
-              they exceeded all expectations. Their approach was thorough,
-              strategic, and tailored to our unique needs. They ensured smooth
-              communication throughout the project and delivered on time. The
-              improvements in our operations have been phenomenal. We look
-              forward to future collaborations!
-            </p>
-            <p className="pt-customer-review-caption">
-                <strong style={{color:"black"}}> Rajesh K., </strong> IT Manager, Financial Institution
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="pt-customer-review">
-            <p className="pt-customer-review-description">
-              The team at LVC Solutions went above and beyond to provide us
-              with cutting-edge solutions. Their dedication to understanding our
-              goals and customizing strategies was impressive. From start to
-              finish, they showcased exceptional professionalism and technical
-              expertise. We are now better equipped to serve our clients and
-              expand our reach. Thank you, LVC Solutions!
-            </p>
-            <p className="pt-customer-review-caption">
-               <strong style={{color:"black"}}> Sarah M.,  </strong> COO, Education Sector
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="pt-customer-review">
-            <p className="pt-customer-review-description">
-              LVC Solutions brought unparalleled expertise and innovation to
-              our project. Their tailored approach addressed our specific
-              challenges, and the results exceeded expectations. They don’t just
-              provide services – they deliver value. Their team’s attention to
-              detail and dedication to quality truly stood out. We’re excited to
-              continue working with them!
-            </p>
-            <p className="pt-customer-review-caption">
-               <strong style={{color:"black"}}>  Michael T., </strong> VP of Operations, Retail Company
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="pt-customer-review">
-            <p className="pt-customer-review-description">
-              I cannot speak highly enough of LVC Solutions LLC. They brought
-              clarity, structure, and efficiency to our complex IT needs. Their
-              team’s expertise in delivering tailored solutions was evident from
-              day one. We’ve experienced significant improvements in system
-              performance and security. It’s been a pleasure working with such a
-              professional and innovative team!
-            </p>
-            <p className="pt-customer-review-caption">
-                <strong style={{color:"black"}}>Lisa P., </strong> Director of Technology, Logistics Company
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="pt-customer-review">
-            <p className="pt-customer-review-description">
-              “LVC Solutions LLC completely transformed our operations. Their
-              team delivered a seamless solution tailored to our needs, ensuring
-              efficiency and scalability. From planning to execution, every step
-              was handled with professionalism and care. Their commitment to
-              excellence is unmatched. Thanks to them, we’ve gained a
-              competitive edge in the market. Highly recommend their services!”
-            </p>
-            <p className="pt-customer-review-caption">
-            <strong style={{color:"black"}}> Emily K., </strong> CEO, E-commerce Business
-            </p>
-          </div>
-        </SwiperSlide>
-  
-      </Swiper>
-</div>
-<div className="footer-section">
+      <div>
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={50}
+          navigation={{ clickable: true }}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="pt-customer-review">
+              <p className="pt-customer-review-description">
+                LVC Solutions LLC completely transformed our operations. Their
+                team delivered a seamless solution tailored to our needs,
+                ensuring efficiency and scalability. From planning to execution,
+                every step was handled with professionalism and care. Their
+                commitment to excellence is unmatched. Thanks to them, we’ve
+                gained a competitive edge in the market. Highly recommend their
+                services!
+              </p>
+              <p className="pt-customer-review-caption">
+                <strong style={{ color: "black" }}>John S.,</strong> CEO,
+                E-commerce Business
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="pt-customer-review">
+              <p className="pt-customer-review-description">
+                Partnering with LVC Solutions was the best decision we made for
+                our company. Their team not only understood our requirements but
+                also delivered innovative solutions to optimize our processes.
+                Their responsiveness and expertise set them apart from others.
+                We’ve seen remarkable improvements in productivity and customer
+                satisfaction. Truly a game-changer for us!
+              </p>
+              <p className="pt-customer-review-caption">
+                <strong style={{ color: "black" }}> Emily K., </strong> CTO,
+                Healthcare Firm
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="pt-customer-review">
+              <p className="pt-customer-review-description">
+                We trusted LVC Solutions to revamp our IT infrastructure, and
+                they exceeded all expectations. Their approach was thorough,
+                strategic, and tailored to our unique needs. They ensured smooth
+                communication throughout the project and delivered on time. The
+                improvements in our operations have been phenomenal. We look
+                forward to future collaborations!
+              </p>
+              <p className="pt-customer-review-caption">
+                <strong style={{ color: "black" }}> Rajesh K., </strong> IT
+                Manager, Financial Institution
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="pt-customer-review">
+              <p className="pt-customer-review-description">
+                The team at LVC Solutions went above and beyond to provide us
+                with cutting-edge solutions. Their dedication to understanding
+                our goals and customizing strategies was impressive. From start
+                to finish, they showcased exceptional professionalism and
+                technical expertise. We are now better equipped to serve our
+                clients and expand our reach. Thank you, LVC Solutions!
+              </p>
+              <p className="pt-customer-review-caption">
+                <strong style={{ color: "black" }}> Sarah M., </strong> COO,
+                Education Sector
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="pt-customer-review">
+              <p className="pt-customer-review-description">
+                LVC Solutions brought unparalleled expertise and innovation to
+                our project. Their tailored approach addressed our specific
+                challenges, and the results exceeded expectations. They don’t
+                just provide services – they deliver value. Their team’s
+                attention to detail and dedication to quality truly stood out.
+                We’re excited to continue working with them!
+              </p>
+              <p className="pt-customer-review-caption">
+                <strong style={{ color: "black" }}> Michael T., </strong> VP of
+                Operations, Retail Company
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="pt-customer-review">
+              <p className="pt-customer-review-description">
+                I cannot speak highly enough of LVC Solutions LLC. They brought
+                clarity, structure, and efficiency to our complex IT needs.
+                Their team’s expertise in delivering tailored solutions was
+                evident from day one. We’ve experienced significant improvements
+                in system performance and security. It’s been a pleasure working
+                with such a professional and innovative team!
+              </p>
+              <p className="pt-customer-review-caption">
+                <strong style={{ color: "black" }}>Lisa P., </strong> Director
+                of Technology, Logistics Company
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="pt-customer-review">
+              <p className="pt-customer-review-description">
+                “LVC Solutions LLC completely transformed our operations. Their
+                team delivered a seamless solution tailored to our needs,
+                ensuring efficiency and scalability. From planning to execution,
+                every step was handled with professionalism and care. Their
+                commitment to excellence is unmatched. Thanks to them, we’ve
+                gained a competitive edge in the market. Highly recommend their
+                services!”
+              </p>
+              <p className="pt-customer-review-caption">
+                <strong style={{ color: "black" }}> Emily K., </strong> CEO,
+                E-commerce Business
+              </p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="footer-section">
         <div className="footer-section-flex">
           <div>
             <h2>Transformation starts here</h2>
             <br></br>
             <p>Imagine your future</p>
-            <button 
-            className="Connect-button"
-            onClick={() => {
-              handleNavigation("/contact");
-            }}
-            >Connect With Us</button>
+            <button
+              className="Connect-button"
+              onClick={() => {
+                handleNavigation("/contact");
+              }}
+            >
+              Connect With Us
+            </button>
           </div>
           <div className="divider"></div>
           <div className="footer-section-flex-column">
             <p>FIND OUT MORE</p>
-            <button className="Connect-button-service"
-            onClick={() => {
-              handleNavigation("/services");
-            }}
-            >Our Services</button>
-            <button className="Connect-button-contact "
-            onClick={() => {
-              handleNavigation("/contact");
-            }}
-            >Contact Us</button>
+            <button
+              className="Connect-button-service"
+              onClick={() => {
+                handleNavigation("/services");
+              }}
+            >
+              Our Services
+            </button>
+            <button
+              className="Connect-button-contact "
+              onClick={() => {
+                handleNavigation("/contact");
+              }}
+            >
+              Contact Us
+            </button>
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </main>
   );
 };
