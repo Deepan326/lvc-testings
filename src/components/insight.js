2a8a3c276f13ac093bdf59b6ebef3697fea4f179
krystalg80 <krystalsoccer80@gmail.com>
2025-03-19 16:00:03 -0700

    fixed duplicate

--- src/components/insight.js
+++ src/components/insight.js
@@ -44,7 +44,8 @@ const Insight = () => {
       title: "Website Maintenance and Support",
     },
     {
-      src: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      // Changed this image URL for Website Design
+      src: "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       alt: "Website Design",
       title: "Website Design",
     },
