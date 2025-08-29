b3d190504411984d805b6371a62adf6eba098694
abraham <agarciasantos14@gmail.com>
2025-08-29 16:41:52 -0700

    added title and container for blog page

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,3 +1,4 @@
+import Footer from './footer'
 
 function BlogPage() {
   // example blog posts
@@ -24,8 +25,92 @@ function BlogPage() {
         "We believe in building long-term relationships by providing reliable, scalable, and affordable solutions tailored to client needs."
     }
   ];
+  //images
+  const images = [
+    {
+      src: "https://images.unsplash.com/photo-1726066012699-1c843dad5fd8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "E-commerce Development",
+      title: "E-commerce Development",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Website Growth",
+      title: "Website Growth",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Website Maintenance and Support",
+      title: "Website Maintenance and Support",
+    },
+    {
+      // Changed this image URL for Website Design
+      src: "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Website Design",
+      title: "Website Design",
+    },
+    {
+      src: "https://images.pexels.com/photos/5473302/pexels-photo-5473302.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      alt: "Cybersecurity",
+      title: "Cybersecurity",
+    },
+    {
+      src: "https://images.pexels.com/photos/4508751/pexels-photo-4508751.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      alt: "Cloud",
+      title: "Cloud",
+    },
+    {
+      src: "https://images.pexels.com/photos/4491461/pexels-photo-4491461.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      alt: "Consulting",
+      title: "Consulting",
+    },
+    {
+      src: "https://images.pexels.com/photos/4709289/pexels-photo-4709289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      alt: "Data & Analytics",
+      title: "Data & Analytics",
+    },
+    {
+      src: "https://images.pexels.com/photos/4065158/pexels-photo-4065158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      alt: "Digital Transformation",
+      title: "Digital Transformation",
+    },
+    {
+      src: "https://images.pexels.com/photos/8438979/pexels-photo-8438979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      alt: "Innovation",
+      title: "Innovation",
+    },
+    {
+      src: "https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      alt: "Artificial Intelligence",
+      title: "Artificial Intelligence",
+    },
+  ];
+  const img = [
+    {
+      src: "https://images.pexels.com/photos/4065158/pexels-photo-4065158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      alt: "Cybersecurity",
+      number: "1",
+      title: "Is DeepSeek China's Sputnik Moment?",
+      description: "IS DEEPSEEK",
+    },
+
+    {
+      src: "https://images.pexels.com/photos/3810792/pexels-photo-3810792.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      alt: "Data & Analytics",
+      number: "2",
+      title: "Iot is transforming hospitals into smart environments",
+      description: "IOT FOR HEALTH CARE ",
+    },
+    {
+      src: "https://images.pexels.com/photos/8438979/pexels-photo-8438979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      alt: "Digital Transformation",
+      number: "3",
+      title: "Six Takeaways From a Monumental Week for AI",
+      description: "TAKEAWAYS FROM A DIRECTORY",
+    },
+  ];
+
   return (
-    <main>
+    <main className='blog-page'>
       <div className='blog-section-herobanner'>
         <div classname='blog-container'>
           <div className='blog-content'>
@@ -37,7 +122,24 @@ function BlogPage() {
         </div>
       </div>
 
+      <div className='blogs-container py-5'>
+        <h1
+          style={{ color: '#333', textAlign: 'start' }}
+          className="whatwe-do-herotitle"> Keep Up With Blogs  </h1>
+{/* 
+          <div>
 
+          </div> */}
+      </div>
+
+      {/* <div className='row'>
+        {images.slice(0,6).map((img, index) => {
+          <div key={index} className=''>
+
+          </div>
+        })}
+      </div> */}
+      <Footer />
 
     </main>
     //   <div className="blog-container">
