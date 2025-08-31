9bd5a62e075fb06e831f3026d976e1789f821278
Daniel Ho <daniel.ho337@gmail.com>
2025-08-31 10:57:56 -0700

    Landing Page: - Emptied the "Services We Offer" section

--- src/components/home.js
+++ src/components/home.js
@@ -9,24 +9,6 @@ import SpotLogo from "../assets/lvcSpo.jpg";
 import AcaLogo from "../assets/lvcAca.jpg";
 
 const Home = () => {
-  const [showAllServices, setShowAllServices] = useState(false);
-  
-    const toggleServices = () => {
-      setShowAllServices((prev) => !prev); 
-    };
-  
-    const images = [
-      { src: "https://images.pexels.com/photos/34577/pexels-photo.jpg", alt: "E-commerce Development", title: "E-commerce Development" },
-      { src: "https://images.pexels.com/photos/1181474/pexels-photo-1181474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", alt: "Website Growth", title: "Website Growth" },
-      { src: "https://images.pexels.com/photos/8867247/pexels-photo-8867247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", alt: "Website Maintenance and Support", title: "Website Maintenance and Support" },
-      { src: "https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", alt: "Website Design", title: "Website Design" },
-      { src: "https://images.pexels.com/photos/8247921/pexels-photo-8247921.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", alt: "Cybersecurity", title: "Cybersecurity" },
-      { src: "https://media.istockphoto.com/id/532187422/photo/cloud-uploading-from-mobile-phone-for-file-sharing-and-collaboration.jpg?b=1&s=612x612&w=0&k=20&c=yQsLHA-8AmPjbNcwanBAxMKosyt7e4D1rJPEvMvaShg=", alt: "Cloud", title: "Cloud" },
-      { src: "https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", alt: "Consulting", title: "Consulting" },
-      { src: "https://images.pexels.com/photos/159888/pexels-photo-159888.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", alt: "Data & Analytics", title: "Data & Analytics" },
-      { src: "https://images.pexels.com/photos/16629368/pexels-photo-16629368/free-photo-of-webpage-of-chatgpt-a-prototype-ai-chatbot-is-seen-on-the-website-of-openai-on-a-smartphone-examples-capabilities-and-limitations-are-shown.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", alt: "Artificial Intelligence", title: "Artificial Intelligence" },
-
-    ];
 
   return (
     <main style={{ padding: "20px",marginTop:"50px" }}>
@@ -50,50 +32,20 @@ const Home = () => {
       <img src="https://static.vecteezy.com/system/resources/previews/021/514/726/non_2x/microsoft-software-logo-brand-symbol-with-name-black-design-illustration-free-vector.jpg" alt="Microsoft Logo" className="company-logo" />
       <img src="https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg" alt="Oracle Logo" className="company-logo" />
       <img src="https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg" alt="AWS Logo" className="company-logo" />
-    </div> */}
-      <div className="row services-section-home">
-        <div style={{backgroundColor:"white",}}className="container py-5">
-          <h1  style={{color:"black",}}className="services-herotitle">Services We Offer</h1>
-          <div className="row">
-            {/* Show first 6 images */}
-            {images.slice(0, 6).map((img, index) => (
-              <div key={index} className="col-md-4 position-relative image-container">
-                <img className="w-100 custom-image" src={img.src} alt={img.alt} />
-                <div className="overlay-text">
-                  <h2 >{img.title}</h2>
-                </div>
-              </div>
-            ))}
+      </div> */}
 
-            {/* Show additional images only if `showAllServices` is true */}
-            {showAllServices &&
-              images.slice(6).map((img, index) => (
-                <div key={index} className="col-md-4 position-relative image-container">
-                  <img className="w-100 custom-image" src={img.src} alt={img.alt} />
-                  <div className="overlay-text">
-                    <h2>{img.title}</h2>
-                  </div>
-                </div>
-              ))}
-          </div>
-
-          {/* Toggle Button */}
-          <div className="text-center mt-4">
-            <button className="view-more-btn" onClick={toggleServices}>
-              {showAllServices ? "Show Less" : "View More "}
-            </button> 
-          </div>
-        </div>
-      </div>
+      <section style={{ backgroundColor:"white" }} className="py-5">
+          <h1 style={{color:"black",}}>Section</h1>
+      </section>
  
       {/* Empty Section */}
       <section className="container py-5 section-4">
-        <h1>Section</h1>
+        <h1 style={{color:"white"}}>Section</h1>
       </section>
 
       {/* Empty Section */}
-      <section style={{ fontSize: "14px",color:"000",fontWeight:"500",background:"white" }}>
-        <h1>Section</h1>
+      <section style={{ fontSize: "14px", color:"000", fontWeight:"500", background:"white" }} className="py-5">
+        <h1 style={{color:"black"}}>Section</h1>
       </section>
 
       <div className="section-5">
