be9f16faf3e3659a3bbd350c091a887092266e0d
abraham <agarciasantos14@gmail.com>
2025-09-01 15:31:44 -0700

    pagination blog posts

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,8 +1,10 @@
 import Footer from './footer'
 import '../style.css'
+import { useState } from 'react';
 
 function BlogPage() {
   // example blog posts
+
   const posts = [
     {
       id: 1,
@@ -25,7 +27,7 @@ function BlogPage() {
       content:
         "We believe in building long-term relationships by providing reliable, scalable, and affordable solutions tailored to client needs."
     },
-     {
+    {
       id: 4,
       title: "Welcome to LVC Solutions Blog",
       date: "August 27, 2025",
@@ -45,10 +47,66 @@ function BlogPage() {
       date: "August 10, 2025",
       content:
         "We believe in building long-term relationships by providing reliable, scalable, and affordable solutions tailored to client needs."
+    },
+    {
+      id: 7,
+      title: "Welcome to LVC Solutions Blog",
+      date: "August 27, 2025",
+      content:
+        "At LVC Solutions, we provide innovative technology and business solutions. This blog is where we share updates, tips, and insights."
+    },
+    {
+      id: 8,
+      title: "How Technology Helps Businesses Grow",
+      date: "August 20, 2025",
+      content:
+        "From automation to data-driven decisions, technology is transforming businesses. At LVC Solutions, we help companies adapt and thrive."
+    },
+    {
+      id: 9,
+      title: "Our Commitment to Clients",
+      date: "August 10, 2025",
+      content:
+        "We believe in building long-term relationships by providing reliable, scalable, and affordable solutions tailored to client needs."
+    },
+    {
+      id: 10,
+      title: "Welcome to LVC Solutions Blog",
+      date: "August 27, 2025",
+      content:
+        "At LVC Solutions, we provide innovative technology and business solutions. This blog is where we share updates, tips, and insights."
+    },
+    {
+      id: 11,
+      title: "How Technology Helps Businesses Grow",
+      date: "August 20, 2025",
+      content:
+        "From automation to data-driven decisions, technology is transforming businesses. At LVC Solutions, we help companies adapt and thrive."
+    },
+    {
+      id: 12,
+      title: "Our Commitment to Clients",
+      date: "August 10, 2025",
+      content:
+        "We believe in building long-term relationships by providing reliable, scalable, and affordable solutions tailored to client needs."
+    },
+    {
+      id: 13,
+      title: "Our Commitment to Clients",
+      date: "August 10, 2025",
+      content:
+        "We believe in building long-term relationships by providing reliable, scalable, and affordable solutions tailored to client needs."
     }
   ];
   //images
+
+
   const images = [
+    {
+      src: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Website Maintenance and Support",
+      title: "Website Maintenance and Support",
+    },
     {
       src: "https://images.unsplash.com/photo-1726066012699-1c843dad5fd8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       alt: "E-commerce Development",
@@ -59,13 +117,8 @@ function BlogPage() {
       alt: "Website Growth",
       title: "Website Growth",
     },
+
     {
-      src: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      alt: "Website Maintenance and Support",
-      title: "Website Maintenance and Support",
-    },
-    {
-      // Changed this image URL for Website Design
       src: "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       alt: "Website Design",
       title: "Website Design",
@@ -81,15 +134,16 @@ function BlogPage() {
       title: "Cloud",
     },
     {
-      src: "https://images.pexels.com/photos/4491461/pexels-photo-4491461.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      alt: "Consulting",
-      title: "Consulting",
-    },
-    {
       src: "https://images.pexels.com/photos/4709289/pexels-photo-4709289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       alt: "Data & Analytics",
       title: "Data & Analytics",
     },
+    {
+      src: "https://images.pexels.com/photos/4491461/pexels-photo-4491461.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      alt: "Consulting",
+      title: "Consulting",
+    },
+
     {
       src: "https://images.pexels.com/photos/4065158/pexels-photo-4065158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       alt: "Digital Transformation",
@@ -105,8 +159,6 @@ function BlogPage() {
       alt: "Artificial Intelligence",
       title: "Artificial Intelligence",
     },
-  ];
-  const img = [
     {
       src: "https://images.pexels.com/photos/4065158/pexels-photo-4065158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       alt: "Cybersecurity",
@@ -114,7 +166,6 @@ function BlogPage() {
       title: "Is DeepSeek China's Sputnik Moment?",
       description: "IS DEEPSEEK",
     },
-
     {
       src: "https://images.pexels.com/photos/3810792/pexels-photo-3810792.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       alt: "Data & Analytics",
@@ -128,13 +179,28 @@ function BlogPage() {
       number: "3",
       title: "Six Takeaways From a Monumental Week for AI",
       description: "TAKEAWAYS FROM A DIRECTORY",
-    },
+    }
   ];
+
+  let [pageNumber, setPageNumber] = useState(1)
+  let maxPages = Math.ceil(posts.length / 6)
+  let offset = (pageNumber - 1) * 6
+
+  console.log(pageNumber)
+
+  //load different pages
+  function pages(arrow) {
+    if (arrow === 'left' && pageNumber > 1) {
+      setPageNumber(prev => prev - 1)
+    } else if (arrow === 'right' && pageNumber < maxPages) {
+      setPageNumber(prev => prev + 1)
+    }
+  }
 
   return (
     <main className='blog-page'>
       <div className='blog-section-herobanner'>
-        <div classname='blog-container'>
+        <div className='blog-container'>
           <div className='blog-content'>
             <p className="blog-subtitle">Blog</p>
             <hr className="underline" />
@@ -151,43 +217,42 @@ function BlogPage() {
         <hr className="underline-2" />
 
         <div className='blog-posts-container'>
-          {posts.map((post, index) => (
+          {posts.slice(offset, offset + 6).map((post) => (
             <article key={post.id} className="blog-posts">
-              <img className='blog-posts-imgs' src={images[index].src} alt={images[index].alt} />
+              <img className='blog-posts-imgs' src={images[post.id - 1].src} alt={images[post.id - 1].alt} />
               <h2>{post.title}</h2>
               <small>{post.date}</small>
               <p>{post.content}</p>
             </article>
           ))}
         </div>
-      </div>
-
-      {/* <div className='row'>
-        {images.slice(0,6).map((img, index) => {
-          <div key={index} className=''>
-
+        <div className='blog-arrows'>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50" height="50"
+            fill="currentColor"
+            className="bi bi-arrow-left-short"
+            viewBox="0 0 16 16"
+            onClick={() => pages('left')}
+          >
+            <path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5" />
+          </svg>
+          <div className='blogs-page-number'>
+            {pageNumber}
           </div>
-        })}
-      </div> */}
+          <svg xmlns="http://www.w3.org/2000/svg"
+            width="50" height="50"
+            fill="currentColor"
+            className="bi bi-arrow-right-short"
+            viewBox="0 0 16 16"
+            onClick={() => pages('right')}
+          >
+            <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+          </svg>
+        </div>
+      </div>
       <Footer />
     </main>
-    //   <div className="blog-container">
-    //   <header className="blog-header">
-    //     <h1>LVC Solutions Blog</h1>
-    //     <p>Insights, updates, and ideas from our team.</p>
-    //   </header>
-
-    //   <main className="blog-content">
-    //        {/* basic blog post mappings ..... */}
-    //     {posts.map((post) => (
-    //       <article key={post.id} className="blog-post">
-    //         <h2>{post.title}</h2>
-    //         <small>{post.date}</small>
-    //         <p>{post.content}</p>
-    //       </article>
-    //     ))}
-    //   </main>
-    // </div>
   );
 }
 
