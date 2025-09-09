b693b31e4da3e1beb616586b28c2804b5deee054
Daniel Ho <daniel.ho337@gmail.com>
2025-09-08 17:12:01 -0700

    Just a minor change.
    Line 122 - Erased an empty space in h1.

--- src/components/services.js
+++ src/components/services.js
@@ -119,7 +119,7 @@ const Home = () => {
 
 
       <div className="section-5">
-        <h1 style={{ textAlign:"center", color:"white" }}>Transform your Goals into reality </h1>
+        <h1 style={{ textAlign:"center", color:"white" }}>Transform your Goals into reality</h1>
       </div>
       <div className="container py-5">
         <div className="row">
