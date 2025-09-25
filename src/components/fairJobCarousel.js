6fc528fc1a3b7829c7dd1230f4814b2e9b5fa057
Daniel Ho <daniel.ho337@gmail.com>
2025-09-24 17:25:38 -0700

    Move the classes "fairjob-carousel-items-container" into their corresponding Carousel.Item tag.

--- src/components/fairJobCarousel.js
+++ src/components/fairJobCarousel.js
@@ -9,8 +9,8 @@ import Row from 'react-bootstrap/Row';
 function FairJobCarousel() {
     return (
         <Carousel className="gradient-background-color bg-black">
-            <Carousel.Item className="fairjob-carousel-scroll">
-                <div className="fairjob-carousel-items-container">
+            <Carousel.Item className="fairjob-carousel-scroll fairjob-carousel-items-container">
+                <div>
                     <div className="row align-items-center ">
                         <div className="col-12 col-md-5" style={{ display: 'flex', flexDirection: 'column', alignItems: 'end'}}>
                             <img
@@ -61,8 +61,8 @@ function FairJobCarousel() {
                 </div>
             </Carousel.Item>
 
-            <Carousel.Item className="fairjob-carousel-scroll">
-                <div className="fairjob-carousel-items-container">
+            <Carousel.Item className="fairjob-carousel-scroll fairjob-carousel-items-container">
+                <div>
                     <Row xs={1} md={1} lg={1} xl={2} className="g-4" style={{ "margin-left": "100px", "margin-right": "100px" }}>
                         <Col>
                             <Card className='fairjob-cards' id="fairjob-card-1">
@@ -121,8 +121,8 @@ function FairJobCarousel() {
                 </div>
             </Carousel.Item>
 
-            <Carousel.Item className="fairjob-carousel-scroll">
-                <div className="fairjob-carousel-items-container">
+            <Carousel.Item className="fairjob-carousel-scroll fairjob-carousel-items-container">
+                <div>
                     <h3 id="fairjob-item-3-h3" style={{ color: 'white', marginBottom: "25px" }} className="header-1-clamp">Why Choose LVC Fair Job?</h3>
                     <ul style={{ textAlign: 'start'}} className="paragraph-clamp">
                         <li> Scam-Free Ecosystem → 100% verified recruiters, job seekers, and postings</li>
@@ -134,8 +134,8 @@ function FairJobCarousel() {
                 </div>
             </Carousel.Item>
 
-            <Carousel.Item className="fairjob-carousel-scroll ">
-                <div className="fairjob-carousel-items-container">
+            <Carousel.Item className="fairjob-carousel-scroll fairjob-carousel-items-container">
+                <div>
                     <h2 style={{ color: 'white', fontWeight: 'bolder', fontSize: 'clamp(10px, 3vmax, 80px)', textAlign: 'center' }}>Competitor Comparison</h2>
                     <p style={{ color: 'white', fontSize: 'clamp(13px, 1vmax, 26px)', textAlign: 'center' }}>Why LVC Fair Job Portal Beats the Rest</p>
                     <table className='spotlight-slide2-grid'>
@@ -210,8 +210,8 @@ function FairJobCarousel() {
                 </div>
             </Carousel.Item>
 
-            <Carousel.Item className="fairjob-carousel-scroll">
-                <div className="fairjob-carousel-items-container">
+            <Carousel.Item className="fairjob-carousel-scroll fairjob-carousel-items-container">
+                <div>
                     <h3 style={{ color: 'white' , "marginBottom": "25px" }} className="header-1-clamp">Join the World's First Scam-Free Job Portal!</h3>
                     <Button href="https://www.lvcfairjob.com/" className="header-2-clamp"> Explore Fair Job Portal</Button>
                 </div>
