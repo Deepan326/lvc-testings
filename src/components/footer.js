3f32194307b4c52476beb0b139a98ca1d8931360
Santhoshi <sheetysantu9620@gmail.com>
2026-02-27 10:40:32 +0530

    changes in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,14 +11,14 @@ const Footer = () => {
     <footer
       style={{
         backgroundColor: "#0000",
-        color: "#fff",
+        color: "#fff", 
         padding: "10px",
 
         bottom: 0,
         width: "100%",
       }}
     >
-      <div className='social-media-container-footer'>
+      {/* <div className='social-media-container-footer'>
         <div>
           <a href="https://x.com/lvcsolutions?s=21" className="twitter-social" target="_blank" rel="noreferrer">
             <img alt='twitter button that redirects to LVC twitter account' className='twitter-media-imgs' src={twitter}/>
@@ -34,26 +34,31 @@ const Footer = () => {
             <img alt='Instagram button that redirects to LVC Instagram account' className='instagram-media-imgs' src={ig}/>
           </a>
         </div>
-      </div>
-      <div className="container my-5">
+      </div> */}
+
+      <div className="container mt-5 mb-0">
         <footer className="text-center text-lg-start text-white" >
           <section>
             <div className="container text-center text-md-start mt-5">
               <div className="row mt-3">
-                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                  <h6 className="text-uppercase fw-bold">LVC Solutions</h6>
-                  <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} />
-                  <p>
-                    <Link to="/" onClick={() => window.scrollTo(0, 0)} style={{ display: 'inline-block' }}>
-                      <img src={logo} alt="Company Logo" className="logo" />
+                <div className="col-12 text-center mb-4">
+                  <div className="footer-brand">
+                    <Link to="/" onClick={() => window.scrollTo(0, 0)} className="d-flex align-items-center" style={{ textDecoration: 'none', color: 'inherit' }}>
+                      <img
+                        src={logo}
+                        alt="Company Logo"
+                        className="footer-logo-small"
+                        // style={{ width: '72px', height: 'auto' }}
+                      />
+                      {/* <span className="footer-brand-text">LVC Solutions</span> */}
                     </Link>
-                  </p>
+                  </div>
                 </div>
 
-                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                  <h6 className="text-uppercase fw-bold">Social Network</h6>
-                  <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} />
-                  <p>
+                {/* <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4"> */}
+                  {/* <h6 className="text-uppercase fw-bold">Social Network</h6>
+                  <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} /> */}
+                  {/* <p>
                     <a href="https://x.com/lvcsolutions?s=21" className="text-white" target="_blank" rel="noreferrer">
                       X
                     </a>
@@ -67,64 +72,55 @@ const Footer = () => {
                     <a href="https://www.instagram.com/lvcsolutions?igsh=Y2Y4OG92eDlnNmh4&utm_source=qr" className="text-white" target="_blank" rel="noreferrer">
                       Instagram
                     </a>
-                  </p>
+                  </p> */}
                   {/* <p>
                     <a href="#!" className="text-white">
                     Facebook
                     </a>
                   </p> */}
+                {/* </div> */}
+                
+                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-1 quick-links">
+                  <h6 className="fw-bold">Quick Links</h6>
+                  {/* <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} /> */}
+                  <p>
+                    <a href="/" >
+                      About
+                    </a>
+                  </p>
+                  <p>
+                    <a href="https://lvcspotlightmedia.com/" target="_blank" rel="noopener noreferrer" >
+                     LVC Spotlight Media
+                    </a>
+                  </p>
+                  <p>
+                    <a href="https://www.lvcfairjob.com/" target="_blank" rel="noopener noreferrer">
+                      LVC Fair Job Portal
+                    </a>
+                  </p>
+                  <p>
+                    <a href="https://www.lvc360.com/Terms#Terms" target="_blank" rel="noopener noreferrer">
+                  Terms
+                    </a>
+                  </p>
+                  <p>
+                    <a href="https://www.lvc360.com/Privacy#Privacy" target="_blank" rel="noopener noreferrer">
+                      Privacy
+                    </a>
+                  </p>
                 </div>
-                {/*
-                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                  <h6 className="text-uppercase fw-bold">Useful links</h6>
-                  <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} />
-                  <p>
-                    <a href="/" className="text-white">
-                      Home
-                    </a>
-                  </p>
-                  <p>
-                    <a href="/about" className="text-white">
-                     Who we are
-                    </a>
-                  </p>
-                  <p>
-                    <a href="/services" className="text-white">
-                      Services
-                    </a>
-                  </p>
-                  <p>
-                    <a href="/insight" className="text-white">
-                  Insights
-                    </a>
-                  </p>
-                  <p>
-                    <a href="/projects" className="text-white">
-                      Projects
-                    </a>
-                  </p>
-                  <p>
-                    <a href="/careers" className="text-white">
-                      Careers
-                    </a>
-                  </p>
-                  <p>
-                    <a href="/contact" className="text-white">
-                      Contact us
-                    </a>
-                  </p>
-                </div> */}
-
-                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                  <h6 className="text-uppercase fw-bold">Contact</h6>
-                  <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} />
+          
+                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-1"  >
+                  <h6 className="fw-bold">Contact Us</h6>
+                  {/* <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} /> */}
                   <p>
                     <i className="fas fa-home mr-3"></i> 5015 Addison Circle,<br/>
-                    Addison, TX 75001, United States
+                    Addison, TX 75001, USA
                   </p>
                   <p>
-                    <i className="fas fa-home mr-3"></i> 228 Hamilton Ave 3rd Floor,<br/>
-                    Palo Alto, CA 94301, United States
+                    <i className="fas fa-home mr-3"></i> 228 Hamilton Avenue
+                     3rd Floor,<br/>
+                    Palo Alto, CA 94301, USA
                   </p>
                   {/* <p>
                     <i className="fas fa-envelope mr-3"></i> info@example.com
@@ -138,12 +134,28 @@ const Footer = () => {
                 </div>
               </div>
             </div>
+            <div className='social-media-container-footer'>
+              <div>
+                <a href="https://x.com/lvcsolutions?s=21" className="twitter-social" target="_blank" rel="noreferrer">
+                  <img alt='twitter button that redirects to LVC twitter account' className='twitter-media-imgs' src={twitter}/>
+                </a>
+              </div>
+              <div>
+                <a href="https://www.linkedin.com/company/lvc-solutions-llc" className="linkedIn-social" target="_blank" rel="noreferrer">
+                  <img alt='linkedIn button that redirects to LVC linkedIn account' className='linkedIn-media-imgs' src={linkedIn}/>
+                </a>
+              </div>
+              <div>
+                <a href="https://www.instagram.com/lvcsolutions?igsh=Y2Y4OG92eDlnNmh4&utm_source=qr" className="instagram-social" target="_blank" rel="noreferrer">
+                  <img alt='Instagram button that redirects to LVC Instagram account' className='instagram-media-imgs' src={ig}/>
+                </a>
+              </div>
+            </div>
           </section>
-          <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-            © 2025 Copyright:
-            <a className="text-white" href="/">
-              Lvc-solutions.com
-            </a>
+          <hr className="footer-hr"/>
+          <div className="text-center p-2" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)", paddingBottom: "0", marginBottom: "0" }}>
+            <p>LVC Solutions</p>
+            <p>All rights reserved.</p>
           </div>
         </footer>
       </div>
